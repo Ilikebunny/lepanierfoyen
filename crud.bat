f54b49347eb59da73c579d4604e4072e1bb28db7
Ilikebunny <larrue.jerome@gmail.com>
2016-08-03 11:12:21 +0200

    Crud Batch
    Batch to use TritonCrud on all entities.

---
+++ crud.bat
@@ -0,0 +1,15 @@
+php bin/console triton:generate:crud --entity=PanierfoyenBundle:Categories --format=annotation --with-write --template=layout.html.twig --overwrite --no-interaction
+php bin/console triton:generate:crud --entity=PanierfoyenBundle:Frequences --format=annotation --with-write --template=layout.html.twig --overwrite --no-interaction
+php bin/console triton:generate:crud --entity=PanierfoyenBundle:Tags --format=annotation --with-write --template=layout.html.twig --overwrite --no-interaction
+php bin/console triton:generate:crud --entity=PanierfoyenBundle:Articles --format=annotation --with-write --template=layout.html.twig --overwrite --no-interaction
+php bin/console triton:generate:crud --entity=PanierfoyenBundle:Coordinateurs --format=annotation --with-write --template=layout.html.twig --overwrite --no-interaction
+php bin/console triton:generate:crud --entity=PanierfoyenBundle:Producteurs --format=annotation --with-write --template=layout.html.twig --overwrite --no-interaction
+php bin/console triton:generate:crud --entity=PanierfoyenBundle:Produits --format=annotation --with-write --template=layout.html.twig --overwrite --no-interaction
+php bin/console triton:generate:crud --entity=PanierfoyenBundle:Conditionnements --format=annotation --with-write --template=layout.html.twig --overwrite --no-interaction
+php bin/console triton:generate:crud --entity=PanierfoyenBundle:Contrats --format=annotation --with-write --template=layout.html.twig --overwrite --no-interaction
+php bin/console triton:generate:crud --entity=PanierfoyenBundle:Payements --format=annotation --with-write --template=layout.html.twig --overwrite --no-interaction
+php bin/console triton:generate:crud --entity=PanierfoyenBundle:ContratsPayements --format=annotation --with-write --template=layout.html.twig --overwrite --no-interaction
+php bin/console triton:generate:crud --entity=PanierfoyenBundle:Lieus --format=annotation --with-write --template=layout.html.twig --overwrite --no-interaction
+php bin/console triton:generate:crud --entity=PanierfoyenBundle:Disponibilites --format=annotation --with-write --template=layout.html.twig --overwrite --no-interaction
+php bin/console triton:generate:crud --entity=PanierfoyenBundle:Distributions --format=annotation --with-write --template=layout.html.twig --overwrite --no-interaction
+pause
