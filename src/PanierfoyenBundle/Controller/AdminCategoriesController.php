f10fb9eede38342b62402b242eb6a91f454a83cb
Ilikebunny <larrue.jerome@gmail.com>
2016-08-09 17:18:19 +0200

    Separating admin / public controller categories

---
+++ src/PanierfoyenBundle/Controller/AdminCategoriesController.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace PanierfoyenBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Pagerfanta\Pagerfanta;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\View\TwitterBootstrap3View;
+use PanierfoyenBundle\Entity\Categories;
+use PanierfoyenBundle\Form\CategoriesType;
+
+/**
+ * Categories controller.
+ *
+ * @Route("/admin/categories")
+ */
+class AdminCategoriesController extends Controller {
+
+    /**
+     * Lists all Categories entities.
+     *
+     * @Route("/", name="admin_categories")
+     * @Method("GET")
+     */
+    public function indexAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $queryBuilder = $em->getRepository('PanierfoyenBundle:Categories')->createQueryBuilder('e');
+
+        list($categories, $pagerHtml) = $this->paginator($queryBuilder, $request);
+
+        return $this->render('categories/admin/index.html.twig', array(
+                    'categories' => $categories,
+                    'pagerHtml' => $pagerHtml,
+        ));
+    }
+
+    /**
+     * Get results from paginator and get paginator view.
+     *
+     */
+    protected function paginator($queryBuilder, $request) {
+        // Paginator
+        $adapter = new DoctrineORMAdapter($queryBuilder);
+        $pagerfanta = new Pagerfanta($adapter);
+        $currentPage = $request->get('page', 1);
+        $pagerfanta->setCurrentPage($currentPage);
+        $entities = $pagerfanta->getCurrentPageResults();
+
+        // Paginator - route generator
+        $me = $this;
+        $routeGenerator = function($page) use ($me) {
+            return $me->generateUrl('categories', array('page' => $page));
+        };
+
+        // Paginator - view
+        $view = new TwitterBootstrap3View();
+        $pagerHtml = $view->render($pagerfanta, $routeGenerator, array(
+            'proximity' => 3,
+            'prev_message' => 'previous',
+            'next_message' => 'next',
+        ));
+
+        return array($entities, $pagerHtml);
+    }
+
+    /**
+     * Displays a form to create a new Categories entity.
+     *
+     * @Route("/new", name="categories_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request) {
+
+        $category = new Categories();
+        $form = $this->createForm('PanierfoyenBundle\Form\CategoriesType', $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('categories_show', array('id' => $category->getId()));
+        }
+        return $this->render('categories/admin/new.html.twig', array(
+                    'category' => $category,
+                    'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Categories entity.
+     *
+     * @Route("/{id}/edit", name="categories_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Categories $category) {
+        $deleteForm = $this->createDeleteForm($category);
+        $editForm = $this->createForm('PanierfoyenBundle\Form\CategoriesType', $category);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('success', 'Edited Successfully!');
+            return $this->redirectToRoute('categories_edit', array('id' => $category->getId()));
+        }
+        return $this->render('categories/admin/edit.html.twig', array(
+                    'category' => $category,
+                    'edit_form' => $editForm->createView(),
+                    'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Categories entity.
+     *
+     * @Route("/{id}", name="categories_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Categories $category) {
+
+        $form = $this->createDeleteForm($category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($category);
+            $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'flash.delete.success');
+        } else {
+            $this->get('session')->getFlashBag()->add('error', 'flash.delete.error');
+        }
+
+        return $this->redirectToRoute('categories');
+    }
+
+    /**
+     * Creates a form to delete a Categories entity.
+     *
+     * @param Categories $category The Categories entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Categories $category) {
+        return $this->createFormBuilder()
+                        ->setAction($this->generateUrl('categories_delete', array('id' => $category->getId())))
+                        ->setMethod('DELETE')
+                        ->getForm()
+        ;
+    }
+
+    /**
+     * Delete Categories by id
+     *
+     * @param mixed $id The entity id
+     * @Route("/delete/{id}", name="categories_by_id_delete")
+     * @Method("GET")
+     */
+    public function deleteById($id) {
+
+        $em = $this->getDoctrine()->getManager();
+        $category = $em->getRepository('PanierfoyenBundle:Categories')->find($id);
+
+        if (!$category) {
+            throw $this->createNotFoundException('Unable to find Categories entity.');
+        }
+
+        try {
+            $em->remove($category);
+            $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'flash.delete.success');
+        } catch (Exception $ex) {
+            $this->get('session')->getFlashBag()->add('error', 'flash.delete.error');
+        }
+
+        return $this->redirect($this->generateUrl('categories'));
+    }
+
+}
--- src/PanierfoyenBundle/Controller/CategoriesController.php
+++ src/PanierfoyenBundle/Controller/CategoriesController.php
@@ -10,45 +10,39 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Pagerfanta\Pagerfanta;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\View\TwitterBootstrap3View;
-
 use PanierfoyenBundle\Entity\Categories;
 use PanierfoyenBundle\Form\CategoriesType;
-
 
 /**
  * Categories controller.
  *
  * @Route("/categories")
  */
-class CategoriesController extends Controller
-{
+class CategoriesController extends Controller {
+
     /**
      * Lists all Categories entities.
      *
      * @Route("/", name="categories")
      * @Method("GET")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
         $queryBuilder = $em->getRepository('PanierfoyenBundle:Categories')->createQueryBuilder('e');
 
         list($categories, $pagerHtml) = $this->paginator($queryBuilder, $request);
-        
-        return $this->render('categories/index.html.twig', array(
-            'categories' => $categories,
-            'pagerHtml' => $pagerHtml,
 
+        return $this->render('categories/index.html.twig', array(
+                    'categories' => $categories,
+                    'pagerHtml' => $pagerHtml,
         ));
     }
 
-
     /**
-    * Get results from paginator and get paginator view.
-    *
-    */
-    protected function paginator($queryBuilder, $request)
-    {
+     * Get results from paginator and get paginator view.
+     *
+     */
+    protected function paginator($queryBuilder, $request) {
         // Paginator
         $adapter = new DoctrineORMAdapter($queryBuilder);
         $pagerfanta = new Pagerfanta($adapter);
@@ -58,8 +52,7 @@ class CategoriesController extends Controller
 
         // Paginator - route generator
         $me = $this;
-        $routeGenerator = function($page) use ($me)
-        {
+        $routeGenerator = function($page) use ($me) {
             return $me->generateUrl('categories', array('page' => $page));
         };
 
@@ -73,152 +66,17 @@ class CategoriesController extends Controller
 
         return array($entities, $pagerHtml);
     }
-    
-    
 
-    /**
-     * Displays a form to create a new Categories entity.
-     *
-     * @Route("/new", name="categories_new")
-     * @Method({"GET", "POST"})
-     */
-    public function newAction(Request $request)
-    {
-    
-        $category = new Categories();
-        $form   = $this->createForm('PanierfoyenBundle\Form\CategoriesType', $category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($category);
-            $em->flush();
-
-            return $this->redirectToRoute('categories_show', array('id' => $category->getId()));
-        }
-        return $this->render('categories/new.html.twig', array(
-            'category' => $category,
-            'form'   => $form->createView(),
-        ));
-    }
-    
-    
-
-    
     /**
      * Finds and displays a Categories entity.
      *
      * @Route("/{id}", name="categories_show")
      * @Method("GET")
      */
-    public function showAction(Categories $category)
-    {
-        $deleteForm = $this->createDeleteForm($category);
+    public function showAction(Categories $category) {
         return $this->render('categories/show.html.twig', array(
-            'category' => $category,
-            'delete_form' => $deleteForm->createView(),
+                    'category' => $category,
         ));
     }
-    
-    
 
-    /**
-     * Displays a form to edit an existing Categories entity.
-     *
-     * @Route("/{id}/edit", name="categories_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function editAction(Request $request, Categories $category)
-    {
-        $deleteForm = $this->createDeleteForm($category);
-        $editForm = $this->createForm('PanierfoyenBundle\Form\CategoriesType', $category);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($category);
-            $em->flush();
-            
-            $this->get('session')->getFlashBag()->add('success', 'Edited Successfully!');
-            return $this->redirectToRoute('categories_edit', array('id' => $category->getId()));
-        }
-        return $this->render('categories/edit.html.twig', array(
-            'category' => $category,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-    
-    
-
-    /**
-     * Deletes a Categories entity.
-     *
-     * @Route("/{id}", name="categories_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, Categories $category)
-    {
-    
-        $form = $this->createDeleteForm($category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($category);
-            $em->flush();
-            $this->get('session')->getFlashBag()->add('success', 'flash.delete.success');
-        } else {
-            $this->get('session')->getFlashBag()->add('error', 'flash.delete.error');
-        }
-        
-        return $this->redirectToRoute('categories');
-    }
-    
-    /**
-     * Creates a form to delete a Categories entity.
-     *
-     * @param Categories $category The Categories entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Categories $category)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('categories_delete', array('id' => $category->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
-    }
-    
-    /**
-     * Delete Categories by id
-     *
-     * @param mixed $id The entity id
-     * @Route("/delete/{id}", name="categories_by_id_delete")
-     * @Method("GET")
-     */
-    public function deleteById($id){
-
-        $em = $this->getDoctrine()->getManager();
-        $category = $em->getRepository('PanierfoyenBundle:Categories')->find($id);
-        
-        if (!$category) {
-            throw $this->createNotFoundException('Unable to find Categories entity.');
-        }
-        
-        try {
-            $em->remove($category);
-            $em->flush();
-            $this->get('session')->getFlashBag()->add('success', 'flash.delete.success');
-        } catch (Exception $ex) {
-            $this->get('session')->getFlashBag()->add('error', 'flash.delete.error');
-        }
-
-        return $this->redirect($this->generateUrl('categories'));
-
-    }
-    
-    
-    
 }
