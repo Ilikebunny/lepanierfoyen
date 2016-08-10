90dd33814d628bab7319eb087e3f2e3df7639f28
Ilikebunny <larrue.jerome@gmail.com>
2016-08-10 09:30:02 +0200

    Backend : admin dashboard

---
+++ src/PanierfoyenBundle/Controller/AdminController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace PanierfoyenBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Pagerfanta\Pagerfanta;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\View\TwitterBootstrap3View;
+use PanierfoyenBundle\Entity\Categories;
+use PanierfoyenBundle\Form\CategoriesType;
+
+/**
+ * Admin controller.
+ *
+ * @Route("/admin")
+ */
+class AdminController extends Controller {
+
+    /**
+     * Lists all Categories entities.
+     *
+     * @Route("/", name="admin_dashboard")
+     * @Method("GET")
+     */
+    public function indexAction(Request $request) {
+        return $this->render('admin_dashboard.html.twig');
+    }
+}
