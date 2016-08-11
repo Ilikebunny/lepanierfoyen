c53fae7b90e81fe9c0caf4b062bbc93d210a9efc
Ilikebunny <larrue.jerome@gmail.com>
2016-08-11 15:46:14 +0200

    Import CSV service

---
+++ src/PanierfoyenBundle/ImportCSV/ImportCSV.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace PanierfoyenBundle\ImportCSV;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ImportCSV {
+
+    private $container;
+
+    public function __construct(ContainerInterface $container) {
+        $this->container = $container;
+    }
+
+    /**
+     * Get csv directory
+     *
+     */
+    private function getDirectoryCSV() {
+        $root = $this->container->getParameter('kernel.root_dir');
+        $root .= "/csv/";
+        return $root;
+    }
+
+    /**
+     * Test import
+     *
+     */
+    public function CSV_to_array($filename) {
+        $row = 1;
+        $csvDirectory = $this->getDirectoryCSV();
+        $csvContent = array();
+        if (($handle = fopen($csvDirectory . $filename, "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+                $csvContent[$row] = $data;
+                $row++;
+            }
+            fclose($handle);
+        }
+        var_dump($csvContent);
+        return $csvContent;
+    }
+
+}
