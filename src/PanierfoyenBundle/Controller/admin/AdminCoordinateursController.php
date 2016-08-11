0da73e007a029ad67eac2a550ef34eac526a4f2e
Ilikebunny <larrue.jerome@gmail.com>
2016-08-11 09:16:07 +0200

    Separating admin / public

--- src/PanierfoyenBundle/Controller/admin/AdminCoordinateursController.php
+++ src/PanierfoyenBundle/Controller/admin/AdminCoordinateursController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PanierfoyenBundle\Controller;
+namespace PanierfoyenBundle\Controller\admin;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -10,30 +10,30 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Pagerfanta\Pagerfanta;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\View\TwitterBootstrap3View;
-use PanierfoyenBundle\Entity\Producteurs;
-use PanierfoyenBundle\Form\ProducteursType;
+use PanierfoyenBundle\Entity\Coordinateurs;
+use PanierfoyenBundle\Form\CoordinateursType;
 
 /**
- * AdminProducteurs controller.
+ * Coordinateurs controller.
  *
- * @Route("/admin/producteurs")
+ * @Route("/admin/coordinateurs")
  */
-class AdminProducteursController extends Controller {
+class AdminCoordinateursController extends Controller {
 
     /**
-     * Lists all Producteurs entities.
+     * Lists all Coordinateurs entities.
      *
-     * @Route("/", name="admin_producteurs")
+     * @Route("/", name="admin_coordinateurs")
      * @Method("GET")
      */
     public function indexAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
-        $queryBuilder = $em->getRepository('PanierfoyenBundle:Producteurs')->createQueryBuilder('e');
+        $queryBuilder = $em->getRepository('PanierfoyenBundle:Coordinateurs')->createQueryBuilder('e');
 
-        list($producteurs, $pagerHtml) = $this->paginator($queryBuilder, $request);
+        list($coordinateurs, $pagerHtml) = $this->paginator($queryBuilder, $request);
 
-        return $this->render('producteurs/admin/index.html.twig', array(
-                    'producteurs' => $producteurs,
+        return $this->render('coordinateurs/admin/index.html.twig', array(
+                    'coordinateurs' => $coordinateurs,
                     'pagerHtml' => $pagerHtml,
         ));
     }
@@ -53,7 +53,7 @@ class AdminProducteursController extends Controller {
         // Paginator - route generator
         $me = $this;
         $routeGenerator = function($page) use ($me) {
-            return $me->generateUrl('producteurs', array('page' => $page));
+            return $me->generateUrl('coordinateurs', array('page' => $page));
         };
 
         // Paginator - view
@@ -68,133 +68,119 @@ class AdminProducteursController extends Controller {
     }
 
     /**
-     * Displays a form to create a new Producteurs entity.
+     * Displays a form to create a new Coordinateurs entity.
      *
-     * @Route("/new", name="producteurs_new")
+     * @Route("/new", name="coordinateurs_new")
      * @Method({"GET", "POST"})
      */
     public function newAction(Request $request) {
 
-        $producteur = new Producteurs();
-        $form = $this->createForm('PanierfoyenBundle\Form\ProducteursType', $producteur);
+        $coordinateur = new Coordinateurs();
+        $form = $this->createForm('PanierfoyenBundle\Form\CoordinateursType', $coordinateur);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            $em->persist($producteur);
+            $em->persist($coordinateur);
             $em->flush();
 
-            return $this->redirectToRoute('producteurs_show', array('id' => $producteur->getId()));
+            return $this->redirectToRoute('coordinateurs_show', array('id' => $coordinateur->getId()));
         }
-        return $this->render('producteurs/admin/new.html.twig', array(
-                    'producteur' => $producteur,
+        return $this->render('coordinateurs/admin/new.html.twig', array(
+                    'coordinateur' => $coordinateur,
                     'form' => $form->createView(),
         ));
     }
 
     /**
-     * Finds and displays a Producteurs entity.
+     * Displays a form to edit an existing Coordinateurs entity.
      *
-     * @Route("/{id}", name="admin_producteurs_show")
-     * @Method("GET")
-     */
-    public function showAction(Producteurs $producteur) {
-        $deleteForm = $this->createDeleteForm($producteur);
-        return $this->render('producteurs/show.html.twig', array(
-                    'producteur' => $producteur,
-                    'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Displays a form to edit an existing Producteurs entity.
-     *
-     * @Route("/{id}/edit", name="producteurs_edit")
+     * @Route("/{id}/edit", name="coordinateurs_edit")
      * @Method({"GET", "POST"})
      */
-    public function editAction(Request $request, Producteurs $producteur) {
-        $deleteForm = $this->createDeleteForm($producteur);
-        $editForm = $this->createForm('PanierfoyenBundle\Form\ProducteursType', $producteur);
+    public function editAction(Request $request, Coordinateurs $coordinateur) {
+        $deleteForm = $this->createDeleteForm($coordinateur);
+        $editForm = $this->createForm('PanierfoyenBundle\Form\CoordinateursType', $coordinateur);
         $editForm->handleRequest($request);
 
         if ($editForm->isSubmitted() && $editForm->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            $em->persist($producteur);
+            $em->persist($coordinateur);
             $em->flush();
 
             $this->get('session')->getFlashBag()->add('success', 'Edited Successfully!');
-            return $this->redirectToRoute('producteurs_edit', array('id' => $producteur->getId()));
+            return $this->redirectToRoute('coordinateurs_edit', array('id' => $coordinateur->getId()));
         }
-        return $this->render('producteurs/admin/edit.html.twig', array(
-                    'producteur' => $producteur,
+        return $this->render('coordinateurs/admin/edit.html.twig', array(
+                    'coordinateur' => $coordinateur,
                     'edit_form' => $editForm->createView(),
                     'delete_form' => $deleteForm->createView(),
         ));
     }
 
     /**
-     * Deletes a Producteurs entity.
+     * Deletes a Coordinateurs entity.
      *
-     * @Route("/{id}", name="producteurs_delete")
+     * @Route("/{id}", name="coordinateurs_delete")
      * @Method("DELETE")
      */
-    public function deleteAction(Request $request, Producteurs $producteur) {
+    public function deleteAction(Request $request, Coordinateurs $coordinateur) {
 
-        $form = $this->createDeleteForm($producteur);
+        $form = $this->createDeleteForm($coordinateur);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            $em->remove($producteur);
+            $em->remove($coordinateur);
             $em->flush();
             $this->get('session')->getFlashBag()->add('success', 'flash.delete.success');
         } else {
             $this->get('session')->getFlashBag()->add('error', 'flash.delete.error');
         }
 
-        return $this->redirectToRoute('producteurs');
+        return $this->redirectToRoute('coordinateurs');
     }
 
     /**
-     * Creates a form to delete a Producteurs entity.
+     * Creates a form to delete a Coordinateurs entity.
      *
-     * @param Producteurs $producteur The Producteurs entity
+     * @param Coordinateurs $coordinateur The Coordinateurs entity
      *
      * @return \Symfony\Component\Form\Form The form
      */
-    private function createDeleteForm(Producteurs $producteur) {
+    private function createDeleteForm(Coordinateurs $coordinateur) {
         return $this->createFormBuilder()
-                        ->setAction($this->generateUrl('producteurs_delete', array('id' => $producteur->getId())))
+                        ->setAction($this->generateUrl('coordinateurs_delete', array('id' => $coordinateur->getId())))
                         ->setMethod('DELETE')
                         ->getForm()
         ;
     }
 
     /**
-     * Delete Producteurs by id
+     * Delete Coordinateurs by id
      *
      * @param mixed $id The entity id
-     * @Route("/delete/{id}", name="producteurs_by_id_delete")
+     * @Route("/delete/{id}", name="coordinateurs_by_id_delete")
      * @Method("GET")
      */
     public function deleteById($id) {
 
         $em = $this->getDoctrine()->getManager();
-        $producteur = $em->getRepository('PanierfoyenBundle:Producteurs')->find($id);
+        $coordinateur = $em->getRepository('PanierfoyenBundle:Coordinateurs')->find($id);
 
-        if (!$producteur) {
-            throw $this->createNotFoundException('Unable to find Producteurs entity.');
+        if (!$coordinateur) {
+            throw $this->createNotFoundException('Unable to find Coordinateurs entity.');
         }
 
         try {
-            $em->remove($producteur);
+            $em->remove($coordinateur);
             $em->flush();
             $this->get('session')->getFlashBag()->add('success', 'flash.delete.success');
         } catch (Exception $ex) {
             $this->get('session')->getFlashBag()->add('error', 'flash.delete.error');
         }
 
-        return $this->redirect($this->generateUrl('producteurs'));
+        return $this->redirect($this->generateUrl('coordinateurs'));
     }
 
 }
--- src/PanierfoyenBundle/Controller/admin/AdminProducteursController.php
+++ src/PanierfoyenBundle/Controller/admin/AdminProducteursController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PanierfoyenBundle\Controller;
+namespace PanierfoyenBundle\Controller\admin;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -10,30 +10,30 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Pagerfanta\Pagerfanta;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\View\TwitterBootstrap3View;
-use PanierfoyenBundle\Entity\Coordinateurs;
-use PanierfoyenBundle\Form\CoordinateursType;
+use PanierfoyenBundle\Entity\Producteurs;
+use PanierfoyenBundle\Form\ProducteursType;
 
 /**
- * Coordinateurs controller.
+ * AdminProducteurs controller.
  *
- * @Route("/admin/coordinateurs")
+ * @Route("/admin/producteurs")
  */
-class AdminCoordinateursController extends Controller {
+class AdminProducteursController extends Controller {
 
     /**
-     * Lists all Coordinateurs entities.
+     * Lists all Producteurs entities.
      *
-     * @Route("/", name="admin_coordinateurs")
+     * @Route("/", name="admin_producteurs")
      * @Method("GET")
      */
     public function indexAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
-        $queryBuilder = $em->getRepository('PanierfoyenBundle:Coordinateurs')->createQueryBuilder('e');
+        $queryBuilder = $em->getRepository('PanierfoyenBundle:Producteurs')->createQueryBuilder('e');
 
-        list($coordinateurs, $pagerHtml) = $this->paginator($queryBuilder, $request);
+        list($producteurs, $pagerHtml) = $this->paginator($queryBuilder, $request);
 
-        return $this->render('coordinateurs/admin/index.html.twig', array(
-                    'coordinateurs' => $coordinateurs,
+        return $this->render('producteurs/admin/index.html.twig', array(
+                    'producteurs' => $producteurs,
                     'pagerHtml' => $pagerHtml,
         ));
     }
@@ -53,7 +53,7 @@ class AdminCoordinateursController extends Controller {
         // Paginator - route generator
         $me = $this;
         $routeGenerator = function($page) use ($me) {
-            return $me->generateUrl('coordinateurs', array('page' => $page));
+            return $me->generateUrl('producteurs', array('page' => $page));
         };
 
         // Paginator - view
@@ -68,119 +68,133 @@ class AdminCoordinateursController extends Controller {
     }
 
     /**
-     * Displays a form to create a new Coordinateurs entity.
+     * Displays a form to create a new Producteurs entity.
      *
-     * @Route("/new", name="coordinateurs_new")
+     * @Route("/new", name="producteurs_new")
      * @Method({"GET", "POST"})
      */
     public function newAction(Request $request) {
 
-        $coordinateur = new Coordinateurs();
-        $form = $this->createForm('PanierfoyenBundle\Form\CoordinateursType', $coordinateur);
+        $producteur = new Producteurs();
+        $form = $this->createForm('PanierfoyenBundle\Form\ProducteursType', $producteur);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            $em->persist($coordinateur);
+            $em->persist($producteur);
             $em->flush();
 
-            return $this->redirectToRoute('coordinateurs_show', array('id' => $coordinateur->getId()));
+            return $this->redirectToRoute('producteurs_show', array('id' => $producteur->getId()));
         }
-        return $this->render('coordinateurs/admin/new.html.twig', array(
-                    'coordinateur' => $coordinateur,
+        return $this->render('producteurs/admin/new.html.twig', array(
+                    'producteur' => $producteur,
                     'form' => $form->createView(),
         ));
     }
 
     /**
-     * Displays a form to edit an existing Coordinateurs entity.
+     * Finds and displays a Producteurs entity.
      *
-     * @Route("/{id}/edit", name="coordinateurs_edit")
+     * @Route("/{id}", name="admin_producteurs_show")
+     * @Method("GET")
+     */
+    public function showAction(Producteurs $producteur) {
+        $deleteForm = $this->createDeleteForm($producteur);
+        return $this->render('producteurs/show.html.twig', array(
+                    'producteur' => $producteur,
+                    'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Producteurs entity.
+     *
+     * @Route("/{id}/edit", name="producteurs_edit")
      * @Method({"GET", "POST"})
      */
-    public function editAction(Request $request, Coordinateurs $coordinateur) {
-        $deleteForm = $this->createDeleteForm($coordinateur);
-        $editForm = $this->createForm('PanierfoyenBundle\Form\CoordinateursType', $coordinateur);
+    public function editAction(Request $request, Producteurs $producteur) {
+        $deleteForm = $this->createDeleteForm($producteur);
+        $editForm = $this->createForm('PanierfoyenBundle\Form\ProducteursType', $producteur);
         $editForm->handleRequest($request);
 
         if ($editForm->isSubmitted() && $editForm->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            $em->persist($coordinateur);
+            $em->persist($producteur);
             $em->flush();
 
             $this->get('session')->getFlashBag()->add('success', 'Edited Successfully!');
-            return $this->redirectToRoute('coordinateurs_edit', array('id' => $coordinateur->getId()));
+            return $this->redirectToRoute('producteurs_edit', array('id' => $producteur->getId()));
         }
-        return $this->render('coordinateurs/admin/edit.html.twig', array(
-                    'coordinateur' => $coordinateur,
+        return $this->render('producteurs/admin/edit.html.twig', array(
+                    'producteur' => $producteur,
                     'edit_form' => $editForm->createView(),
                     'delete_form' => $deleteForm->createView(),
         ));
     }
 
     /**
-     * Deletes a Coordinateurs entity.
+     * Deletes a Producteurs entity.
      *
-     * @Route("/{id}", name="coordinateurs_delete")
+     * @Route("/{id}", name="producteurs_delete")
      * @Method("DELETE")
      */
-    public function deleteAction(Request $request, Coordinateurs $coordinateur) {
+    public function deleteAction(Request $request, Producteurs $producteur) {
 
-        $form = $this->createDeleteForm($coordinateur);
+        $form = $this->createDeleteForm($producteur);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            $em->remove($coordinateur);
+            $em->remove($producteur);
             $em->flush();
             $this->get('session')->getFlashBag()->add('success', 'flash.delete.success');
         } else {
             $this->get('session')->getFlashBag()->add('error', 'flash.delete.error');
         }
 
-        return $this->redirectToRoute('coordinateurs');
+        return $this->redirectToRoute('producteurs');
     }
 
     /**
-     * Creates a form to delete a Coordinateurs entity.
+     * Creates a form to delete a Producteurs entity.
      *
-     * @param Coordinateurs $coordinateur The Coordinateurs entity
+     * @param Producteurs $producteur The Producteurs entity
      *
      * @return \Symfony\Component\Form\Form The form
      */
-    private function createDeleteForm(Coordinateurs $coordinateur) {
+    private function createDeleteForm(Producteurs $producteur) {
         return $this->createFormBuilder()
-                        ->setAction($this->generateUrl('coordinateurs_delete', array('id' => $coordinateur->getId())))
+                        ->setAction($this->generateUrl('producteurs_delete', array('id' => $producteur->getId())))
                         ->setMethod('DELETE')
                         ->getForm()
         ;
     }
 
     /**
-     * Delete Coordinateurs by id
+     * Delete Producteurs by id
      *
      * @param mixed $id The entity id
-     * @Route("/delete/{id}", name="coordinateurs_by_id_delete")
+     * @Route("/delete/{id}", name="producteurs_by_id_delete")
      * @Method("GET")
      */
     public function deleteById($id) {
 
         $em = $this->getDoctrine()->getManager();
-        $coordinateur = $em->getRepository('PanierfoyenBundle:Coordinateurs')->find($id);
+        $producteur = $em->getRepository('PanierfoyenBundle:Producteurs')->find($id);
 
-        if (!$coordinateur) {
-            throw $this->createNotFoundException('Unable to find Coordinateurs entity.');
+        if (!$producteur) {
+            throw $this->createNotFoundException('Unable to find Producteurs entity.');
         }
 
         try {
-            $em->remove($coordinateur);
+            $em->remove($producteur);
             $em->flush();
             $this->get('session')->getFlashBag()->add('success', 'flash.delete.success');
         } catch (Exception $ex) {
             $this->get('session')->getFlashBag()->add('error', 'flash.delete.error');
         }
 
-        return $this->redirect($this->generateUrl('coordinateurs'));
+        return $this->redirect($this->generateUrl('producteurs'));
     }
 
 }
