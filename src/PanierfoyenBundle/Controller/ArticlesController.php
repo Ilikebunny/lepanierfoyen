5bbee003941ba4eaeb1f23bbbbc1027d9e484481
Ilikebunny <larrue.jerome@gmail.com>
2016-08-15 23:29:23 +0200

    Separating admin / public

--- src/PanierfoyenBundle/Controller/ArticlesController.php
+++ src/PanierfoyenBundle/Controller/ArticlesController.php
@@ -10,45 +10,39 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Pagerfanta\Pagerfanta;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\View\TwitterBootstrap3View;
-
 use PanierfoyenBundle\Entity\Articles;
 use PanierfoyenBundle\Form\ArticlesType;
-
 
 /**
  * Articles controller.
  *
  * @Route("/articles")
  */
-class ArticlesController extends Controller
-{
+class ArticlesController extends Controller {
+
     /**
      * Lists all Articles entities.
      *
      * @Route("/", name="articles")
      * @Method("GET")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
         $queryBuilder = $em->getRepository('PanierfoyenBundle:Articles')->createQueryBuilder('e');
 
         list($articles, $pagerHtml) = $this->paginator($queryBuilder, $request);
-        
-        return $this->render('articles/index.html.twig', array(
-            'articles' => $articles,
-            'pagerHtml' => $pagerHtml,
 
+        return $this->render('articles/index.html.twig', array(
+                    'articles' => $articles,
+                    'pagerHtml' => $pagerHtml,
         ));
     }
 
-
     /**
-    * Get results from paginator and get paginator view.
-    *
-    */
-    protected function paginator($queryBuilder, $request)
-    {
+     * Get results from paginator and get paginator view.
+     *
+     */
+    protected function paginator($queryBuilder, $request) {
         // Paginator
         $adapter = new DoctrineORMAdapter($queryBuilder);
         $pagerfanta = new Pagerfanta($adapter);
@@ -58,8 +52,7 @@ class ArticlesController extends Controller
 
         // Paginator - route generator
         $me = $this;
-        $routeGenerator = function($page) use ($me)
-        {
+        $routeGenerator = function($page) use ($me) {
             return $me->generateUrl('articles', array('page' => $page));
         };
 
@@ -73,152 +66,17 @@ class ArticlesController extends Controller
 
         return array($entities, $pagerHtml);
     }
-    
-    
 
-    /**
-     * Displays a form to create a new Articles entity.
-     *
-     * @Route("/new", name="articles_new")
-     * @Method({"GET", "POST"})
-     */
-    public function newAction(Request $request)
-    {
-    
-        $article = new Articles();
-        $form   = $this->createForm('PanierfoyenBundle\Form\ArticlesType', $article);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($article);
-            $em->flush();
-
-            return $this->redirectToRoute('articles_show', array('id' => $article->getId()));
-        }
-        return $this->render('articles/new.html.twig', array(
-            'article' => $article,
-            'form'   => $form->createView(),
-        ));
-    }
-    
-    
-
-    
     /**
      * Finds and displays a Articles entity.
      *
      * @Route("/{id}", name="articles_show")
      * @Method("GET")
      */
-    public function showAction(Articles $article)
-    {
-        $deleteForm = $this->createDeleteForm($article);
+    public function showAction(Articles $article) {
         return $this->render('articles/show.html.twig', array(
-            'article' => $article,
-            'delete_form' => $deleteForm->createView(),
+                    'article' => $article,
         ));
     }
-    
-    
 
-    /**
-     * Displays a form to edit an existing Articles entity.
-     *
-     * @Route("/{id}/edit", name="articles_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function editAction(Request $request, Articles $article)
-    {
-        $deleteForm = $this->createDeleteForm($article);
-        $editForm = $this->createForm('PanierfoyenBundle\Form\ArticlesType', $article);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($article);
-            $em->flush();
-            
-            $this->get('session')->getFlashBag()->add('success', 'Edited Successfully!');
-            return $this->redirectToRoute('articles_edit', array('id' => $article->getId()));
-        }
-        return $this->render('articles/edit.html.twig', array(
-            'article' => $article,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-    
-    
-
-    /**
-     * Deletes a Articles entity.
-     *
-     * @Route("/{id}", name="articles_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, Articles $article)
-    {
-    
-        $form = $this->createDeleteForm($article);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($article);
-            $em->flush();
-            $this->get('session')->getFlashBag()->add('success', 'flash.delete.success');
-        } else {
-            $this->get('session')->getFlashBag()->add('error', 'flash.delete.error');
-        }
-        
-        return $this->redirectToRoute('articles');
-    }
-    
-    /**
-     * Creates a form to delete a Articles entity.
-     *
-     * @param Articles $article The Articles entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Articles $article)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('articles_delete', array('id' => $article->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
-    }
-    
-    /**
-     * Delete Articles by id
-     *
-     * @param mixed $id The entity id
-     * @Route("/delete/{id}", name="articles_by_id_delete")
-     * @Method("GET")
-     */
-    public function deleteById($id){
-
-        $em = $this->getDoctrine()->getManager();
-        $article = $em->getRepository('PanierfoyenBundle:Articles')->find($id);
-        
-        if (!$article) {
-            throw $this->createNotFoundException('Unable to find Articles entity.');
-        }
-        
-        try {
-            $em->remove($article);
-            $em->flush();
-            $this->get('session')->getFlashBag()->add('success', 'flash.delete.success');
-        } catch (Exception $ex) {
-            $this->get('session')->getFlashBag()->add('error', 'flash.delete.error');
-        }
-
-        return $this->redirect($this->generateUrl('articles'));
-
-    }
-    
-    
-    
 }
---
+++ src/PanierfoyenBundle/Controller/admin/AdminArticlesController.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace PanierfoyenBundle\Controller\admin;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Pagerfanta\Pagerfanta;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\View\TwitterBootstrap3View;
+use PanierfoyenBundle\Entity\Articles;
+use PanierfoyenBundle\Form\ArticlesType;
+
+/**
+ * Articles controller.
+ *
+ * @Route("/admin/articles")
+ */
+class AdminArticlesController extends Controller {
+
+    /**
+     * Lists all Articles entities.
+     *
+     * @Route("/", name="admin_articles")
+     * @Method("GET")
+     */
+    public function indexAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $queryBuilder = $em->getRepository('PanierfoyenBundle:Articles')->createQueryBuilder('e');
+
+        list($articles, $pagerHtml) = $this->paginator($queryBuilder, $request);
+
+        return $this->render('articles/admin/index.html.twig', array(
+                    'articles' => $articles,
+                    'pagerHtml' => $pagerHtml,
+        ));
+    }
+
+    /**
+     * Get results from paginator and get paginator view.
+     *
+     */
+    protected function paginator($queryBuilder, $request) {
+        // Paginator
+        $adapter = new DoctrineORMAdapter($queryBuilder);
+        $pagerfanta = new Pagerfanta($adapter);
+        $currentPage = $request->get('page', 1);
+        $pagerfanta->setCurrentPage($currentPage);
+        $entities = $pagerfanta->getCurrentPageResults();
+
+        // Paginator - route generator
+        $me = $this;
+        $routeGenerator = function($page) use ($me) {
+            return $me->generateUrl('articles', array('page' => $page));
+        };
+
+        // Paginator - view
+        $view = new TwitterBootstrap3View();
+        $pagerHtml = $view->render($pagerfanta, $routeGenerator, array(
+            'proximity' => 3,
+            'prev_message' => 'previous',
+            'next_message' => 'next',
+        ));
+
+        return array($entities, $pagerHtml);
+    }
+
+    /**
+     * Displays a form to create a new Articles entity.
+     *
+     * @Route("/new", name="articles_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request) {
+
+        $article = new Articles();
+        $form = $this->createForm('PanierfoyenBundle\Form\ArticlesType', $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('articles_show', array('id' => $article->getId()));
+        }
+        return $this->render('articles/new.html.twig', array(
+                    'article' => $article,
+                    'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Articles entity.
+     *
+     * @Route("/{id}/edit", name="articles_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Articles $article) {
+        $deleteForm = $this->createDeleteForm($article);
+        $editForm = $this->createForm('PanierfoyenBundle\Form\ArticlesType', $article);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('success', 'Edited Successfully!');
+            return $this->redirectToRoute('articles_edit', array('id' => $article->getId()));
+        }
+        return $this->render('articles/edit.html.twig', array(
+                    'article' => $article,
+                    'edit_form' => $editForm->createView(),
+                    'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Articles entity.
+     *
+     * @Route("/{id}", name="articles_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Articles $article) {
+
+        $form = $this->createDeleteForm($article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($article);
+            $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'flash.delete.success');
+        } else {
+            $this->get('session')->getFlashBag()->add('error', 'flash.delete.error');
+        }
+
+        return $this->redirectToRoute('articles');
+    }
+
+    /**
+     * Creates a form to delete a Articles entity.
+     *
+     * @param Articles $article The Articles entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Articles $article) {
+        return $this->createFormBuilder()
+                        ->setAction($this->generateUrl('articles_delete', array('id' => $article->getId())))
+                        ->setMethod('DELETE')
+                        ->getForm()
+        ;
+    }
+
+    /**
+     * Delete Articles by id
+     *
+     * @param mixed $id The entity id
+     * @Route("/delete/{id}", name="articles_by_id_delete")
+     * @Method("GET")
+     */
+    public function deleteById($id) {
+
+        $em = $this->getDoctrine()->getManager();
+        $article = $em->getRepository('PanierfoyenBundle:Articles')->find($id);
+
+        if (!$article) {
+            throw $this->createNotFoundException('Unable to find Articles entity.');
+        }
+
+        try {
+            $em->remove($article);
+            $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'flash.delete.success');
+        } catch (Exception $ex) {
+            $this->get('session')->getFlashBag()->add('error', 'flash.delete.error');
+        }
+
+        return $this->redirect($this->generateUrl('articles'));
+    }
+
+}
