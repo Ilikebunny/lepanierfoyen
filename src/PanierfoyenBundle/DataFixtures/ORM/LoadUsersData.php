411e90c83fb36fe285df791564e3f6203ab5ab6a
Ilikebunny <larrue.jerome@gmail.com>
2016-08-18 08:20:30 +0200

    Data fixtures : users

---
+++ src/PanierfoyenBundle/DataFixtures/ORM/LoadUsersData.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace PanierfoyenBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use PanierfoyenBundle\Entity\Tags;
+
+class LoadUsersData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface {
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null) {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager) {
+
+        $userManager = $this->container->get('fos_user.user_manager');
+
+        $user = $userManager->createUser();
+
+        $user
+                ->setUsername('larrue.jerome')
+                ->setEmail('larrue.jerome@gmail.com')
+//                ->setFirstLogin(\DateTime::createFromFormat('j-M-Y', '15-Feb-2009'))
+                ->setEnabled(true);
+
+        $user->setPlainPassword('somepass');
+
+        // Equivalent à :
+//        $encoder = $this->container
+//                ->get('security.encoder_factory')
+//                ->getEncoder($user)
+//            ;
+//        $user->setPassword($encoder->encodePassword('somepass', $user->getSalt()));
+
+
+        $userManager->updateUser($user);
+    }
+
+    public function getOrder() {
+        // the order in which fixtures will be loaded
+        // the lower the number, the sooner that this fixture is loaded
+        return 10;
+    }
+
+}
