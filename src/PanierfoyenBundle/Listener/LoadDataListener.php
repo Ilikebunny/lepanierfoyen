d96c56a13c261ebd00c53e7825ebace19b821562
Ilikebunny <larrue.jerome@gmail.com>
2016-08-20 09:17:47 +0200

    Testing

--- src/PanierfoyenBundle/Listener/LoadDataListener.php
+++ src/PanierfoyenBundle/Listener/LoadDataListener.php
@@ -5,22 +5,26 @@ namespace PanierfoyenBundle\Listener;
 use AncaRebeca\FullCalendarBundle\Event\CalendarEvent;
 use PanierfoyenBundle\Entity\CalendarEvent as Event;
 
-class LoadDataListener
-{
+class LoadDataListener {
+
     /**
      * @param CalendarEvent $calendarEvent
      *
      * @return EventInterface[]
      */
-    public function loadData(CalendarEvent $calendarEvent)
-    {
-         $startDate = $calendarEvent->getStartDatetime();
-         $endDate = $calendarEvent->getEndDatetime();
-         $filters = $calendarEvent->getFilters();
+    public function loadData(CalendarEvent $calendarEvent) {
+        $startDate = $calendarEvent->getStartDatetime();
+        $endDate = $calendarEvent->getEndDatetime();
+        $filters = $calendarEvent->getFilters();
 
-         //You may want do a custom query to populate the events
+        //You may want do a custom query to populate the events
+        $start = new \DateTime('2016-08-08T17:30');
+        $end = new \DateTime('2016-08-08T19:30');
 
-         $calendarEvent->addEvent(new Event('Event Title 1', new \DateTime()));
-         $calendarEvent->addEvent(new Event('Event Title 2', new \DateTime()));
+        $event = new Event('Event Title 1', $start);
+        $event->setEndDate($end);
+
+        $calendarEvent->addEvent($event);
     }
+
 }
