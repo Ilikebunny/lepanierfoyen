449adc50e2b7f242f666487e32bf3a7f81a9b412
Ilikebunny <larrue.jerome@gmail.com>
2016-08-23 10:49:02 +0200

    User management

--- src/PanierfoyenBundle/Controller/front/UsersController.php
+++ src/PanierfoyenBundle/Controller/front/UsersController.php
@@ -10,31 +10,53 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Pagerfanta\Pagerfanta;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\View\TwitterBootstrap3View;
-
 use PanierfoyenBundle\Entity\Users;
 use PanierfoyenBundle\Form\UsersType;
-
 
 /**
  * Users controller.
  *
  * @Route("/monProfil")
  */
-class UsersController extends Controller
-{
-       
-    
+class UsersController extends Controller {
+
+    /**
+     * Finds and displays a Users entity.
+     *
+     * @Route("/edit", name="myprofil_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editProfilAction(Request $request) {
+        
+        $user = $this->getUser();
+        $editForm = $this->createForm('PanierfoyenBundle\Form\UsersType', $user);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('success', 'Edited Successfully!');
+            return $this->redirectToRoute('myprofil_edit');
+        }
+        return $this->render('users/profil/edit.html.twig', array(
+                    'user' => $user,
+                    'edit_form' => $editForm->createView(),
+        ));
+        
+    }
+
     /**
      * Finds and displays a Users entity.
      *
      * @Route("/{id}", name="users_show")
      * @Method("GET")
      */
-    public function showAction(Users $user)
-    {
+    public function showAction(Users $user) {
         return $this->render('users/show.html.twig', array(
-            'user' => $user,
+                    'user' => $user,
         ));
     }
-      
+
 }
--- src/PanierfoyenBundle/Form/UsersType.php
+++ src/PanierfoyenBundle/Form/UsersType.php
@@ -6,36 +6,29 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class UsersType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class UsersType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('nom')
-            ->add('codepostal')
-            ->add('ville')
-            ->add('adr1')
-            ->add('adr2')
-            ->add('adr3')
-            ->add('tel')
-            ->add('mobile')
-            ->add('role')
-            ->add('created')
-            ->add('modified')
-            ->add('coordinateur')
-            ->add('groups')
+                ->add('nom')
+                ->add('codepostal')
+                ->add('ville')
+                ->add('adr1')
+                ->add('adr2')
+                ->add('adr3')
+                ->add('tel')
+                ->add('mobile')
         ;
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'PanierfoyenBundle\Entity\Users'
         ));
     }
 
-    public function getBlockPrefix()
-    {
+    public function getBlockPrefix() {
         return 'panierfoyenbundle_users';
     }
+
 }
