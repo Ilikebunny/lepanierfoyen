f41e565ba7f2d3633db98dd8ce23dc39653e115a
Ilikebunny <larrue.jerome@gmail.com>
2016-08-23 08:41:23 +0200

    Correcting pagination using custom service

--- src/PanierfoyenBundle/Controller/admin/AdminArticlesController.php
+++ src/PanierfoyenBundle/Controller/admin/AdminArticlesController.php
@@ -7,9 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Pagerfanta\Pagerfanta;
-use Pagerfanta\Adapter\DoctrineORMAdapter;
-use Pagerfanta\View\TwitterBootstrap3View;
+
 use PanierfoyenBundle\Entity\Articles;
 use PanierfoyenBundle\Form\ArticlesType;
 
@@ -28,43 +26,17 @@ class AdminArticlesController extends Controller {
      */
     public function indexAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
-        $queryBuilder = $em->getRepository('PanierfoyenBundle:Articles')->createQueryBuilder('e');
+        $queryBuilder = $em->getRepository('PanierfoyenBundle:Articles')->createQueryBuilder('e')
+                ->orderBy('e.created', 'DESC');
 
-        list($articles, $pagerHtml) = $this->paginator($queryBuilder, $request);
+        //Pagination
+        $paginator = $this->container->get('panierfoyen.paginator');
+        list($articles, $pagerHtml) = $paginator->paginatorSimple($queryBuilder, $request, 10, 'admin_articles');
 
         return $this->render('articles/admin/index.html.twig', array(
                     'articles' => $articles,
                     'pagerHtml' => $pagerHtml,
         ));
-    }
-
-    /**
-     * Get results from paginator and get paginator view.
-     *
-     */
-    protected function paginator($queryBuilder, $request) {
-        // Paginator
-        $adapter = new DoctrineORMAdapter($queryBuilder);
-        $pagerfanta = new Pagerfanta($adapter);
-        $currentPage = $request->get('page', 1);
-        $pagerfanta->setCurrentPage($currentPage);
-        $entities = $pagerfanta->getCurrentPageResults();
-
-        // Paginator - route generator
-        $me = $this;
-        $routeGenerator = function($page) use ($me) {
-            return $me->generateUrl('articles', array('page' => $page));
-        };
-
-        // Paginator - view
-        $view = new TwitterBootstrap3View();
-        $pagerHtml = $view->render($pagerfanta, $routeGenerator, array(
-            'proximity' => 3,
-            'prev_message' => 'previous',
-            'next_message' => 'next',
-        ));
-
-        return array($entities, $pagerHtml);
     }
 
     /**
