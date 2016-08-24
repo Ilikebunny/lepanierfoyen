951a9856de1a9051b085f3bffa98dcba39d24d20
Ilikebunny <larrue.jerome@gmail.com>
2016-08-24 09:51:32 +0200

    Produits / Conditionnements Admin management

--- src/PanierfoyenBundle/Controller/admin/AdminProduitsController.php
+++ src/PanierfoyenBundle/Controller/admin/AdminProduitsController.php
@@ -7,33 +7,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
 use PanierfoyenBundle\Entity\Produits;
+use PanierfoyenBundle\Entity\Conditionnements;
+use PanierfoyenBundle\Form\ConditionnementsType;
 
 /**
  * Produits controller.
  *
  * @Route("/admin/produits")
  */
-class AdminProduitsController extends Controller
-{
+class AdminProduitsController extends Controller {
+
     /**
      * Lists all Produits entities.
      *
      * @Route("/", name="admin_produits")
      * @Method("GET")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
         $queryBuilder = $em->getRepository('PanierfoyenBundle:Produits')->createQueryBuilder('e');
 
         $paginator = $this->container->get('panierfoyen.paginator');
         list($produits, $pagerHtml) = $paginator->paginatorSimple($queryBuilder, $request, 12, 'admin_produits');
-        
+
         return $this->render('produits/admin/index.html.twig', array(
-            'produits' => $produits,
-            'pagerHtml' => $pagerHtml,
+                    'produits' => $produits,
+                    'pagerHtml' => $pagerHtml,
         ));
     }
 
@@ -43,9 +43,8 @@ class AdminProduitsController extends Controller
      * @Route("/new", name="produits_new")
      * @Method({"GET", "POST"})
      */
-    public function newAction(Request $request)
-    {
-    
+    public function newAction(Request $request) {
+
         $produit = new Produits();
         $form = $this->createForm('PanierfoyenBundle\Form\ProduitsType', $produit);
         $form->handleRequest($request);
@@ -58,26 +57,24 @@ class AdminProduitsController extends Controller
             return $this->redirectToRoute('produits_prewiev', array('id' => $produit->getId()));
         }
         return $this->render('produits/admin/new.html.twig', array(
-            'produit' => $produit,
-            'form'   => $form->createView(),
+                    'produit' => $produit,
+                    'form' => $form->createView(),
         ));
     }
-        
+
     /**
      * Finds and displays a Produits entity.
      *
      * @Route("/preview/{id}", name="produits_prewiev")
      * @Method("GET")
      */
-    public function showAction(Produits $produit)
-    {
+    public function showAction(Produits $produit) {
         $deleteForm = $this->createDeleteForm($produit);
         return $this->render('produits/admin/show.html.twig', array(
-            'produit' => $produit,
-            'delete_form' => $deleteForm->createView(),
+                    'produit' => $produit,
+                    'delete_form' => $deleteForm->createView(),
         ));
     }
-    
 
     /**
      * Displays a form to edit an existing Produits entity.
@@ -85,8 +82,7 @@ class AdminProduitsController extends Controller
      * @Route("/{id}/edit", name="produits_edit")
      * @Method({"GET", "POST"})
      */
-    public function editAction(Request $request, Produits $produit)
-    {
+    public function editAction(Request $request, Produits $produit) {
         $deleteForm = $this->createDeleteForm($produit);
         $editForm = $this->createForm('PanierfoyenBundle\Form\ProduitsType', $produit);
         $editForm->handleRequest($request);
@@ -95,17 +91,75 @@ class AdminProduitsController extends Controller
             $em = $this->getDoctrine()->getManager();
             $em->persist($produit);
             $em->flush();
-            
+
             $this->get('session')->getFlashBag()->add('success', 'Edited Successfully!');
             return $this->redirectToRoute('produits_edit', array('id' => $produit->getId()));
         }
         return $this->render('produits/admin/edit.html.twig', array(
-            'produit' => $produit,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+                    'produit' => $produit,
+                    'edit_form' => $editForm->createView(),
+                    'delete_form' => $deleteForm->createView(),
         ));
     }
-    
+
+    /**
+     * Displays a form to edit an existing Produits entity.
+     *
+     * @Route("/conditionnements/{id}/edit", name="admin_produits_conditionnements")
+     * @Method({"GET", "POST"})
+     */
+    public function editConditionnementsAction(Request $request, Produits $produit) {
+
+        $em = $this->getDoctrine()->getManager();
+        $queryBuilder = $em->getRepository('PanierfoyenBundle:Conditionnements')->createQueryBuilder('e');
+
+        $conditionnements = $produit->getLes_conditionnements();
+//        list($conditionnements, $pagerHtml) = $this->paginator($queryBuilder, $request);
+
+        $conditionnement = new Conditionnements();
+        $conditionnement->setProduit($produit);
+        
+        $form = $this->createForm('PanierfoyenBundle\Form\ConditionnementsType', $conditionnement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($conditionnement);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_produits_conditionnements', array(
+//                        'conditionnements' => $conditionnements,
+//                        'produit' => $produit,
+//                        'form' => $form->createView(),
+                        'id' =>$produit->getId(),
+            ));
+        }
+
+        return $this->render('produits/admin/conditionnements.html.twig', array(
+                    'conditionnements' => $conditionnements,
+                    'produit' => $produit,
+                    'form' => $form->createView(),
+                    'temp' => $conditionnement,
+        ));
+
+        $deleteForm = $this->createDeleteForm($produit);
+        $editForm = $this->createForm('PanierfoyenBundle\Form\ProduitsType', $produit);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($produit);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('success', 'Edited Successfully!');
+            return $this->redirectToRoute('produits_edit', array('id' => $produit->getId()));
+        }
+        return $this->render('produits/admin/edit.html.twig', array(
+                    'produit' => $produit,
+                    'edit_form' => $editForm->createView(),
+                    'delete_form' => $deleteForm->createView(),
+        ));
+    }
 
     /**
      * Deletes a Produits entity.
@@ -113,9 +167,8 @@ class AdminProduitsController extends Controller
      * @Route("/{id}", name="produits_delete")
      * @Method("DELETE")
      */
-    public function deleteAction(Request $request, Produits $produit)
-    {
-    
+    public function deleteAction(Request $request, Produits $produit) {
+
         $form = $this->createDeleteForm($produit);
         $form->handleRequest($request);
 
@@ -127,10 +180,10 @@ class AdminProduitsController extends Controller
         } else {
             $this->get('session')->getFlashBag()->add('error', 'flash.delete.error');
         }
-        
+
         return $this->redirectToRoute('admin_produits');
     }
-    
+
     /**
      * Creates a form to delete a Produits entity.
      *
@@ -138,15 +191,14 @@ class AdminProduitsController extends Controller
      *
      * @return \Symfony\Component\Form\Form The form
      */
-    private function createDeleteForm(Produits $produit)
-    {
+    private function createDeleteForm(Produits $produit) {
         return $this->createFormBuilder()
-            ->setAction($this->generateUrl('produits_delete', array('id' => $produit->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
+                        ->setAction($this->generateUrl('produits_delete', array('id' => $produit->getId())))
+                        ->setMethod('DELETE')
+                        ->getForm()
         ;
     }
-    
+
     /**
      * Delete Produits by id
      *
@@ -154,15 +206,15 @@ class AdminProduitsController extends Controller
      * @Route("/delete/{id}", name="produits_by_id_delete")
      * @Method("GET")
      */
-    public function deleteById($id){
+    public function deleteById($id) {
 
         $em = $this->getDoctrine()->getManager();
         $produit = $em->getRepository('PanierfoyenBundle:Produits')->find($id);
-        
+
         if (!$produit) {
             throw $this->createNotFoundException('Unable to find Produits entity.');
         }
-        
+
         try {
             $em->remove($produit);
             $em->flush();
@@ -172,8 +224,6 @@ class AdminProduitsController extends Controller
         }
 
         return $this->redirect($this->generateUrl('produits'));
-
     }
-     
-    
+
 }
