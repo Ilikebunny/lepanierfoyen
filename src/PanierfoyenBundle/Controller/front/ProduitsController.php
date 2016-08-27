5c888a908b34f5b84936d279361d25a4fc56b7af
Ilikebunny <larrue.jerome@gmail.com>
2016-08-27 12:06:48 +0200

    produits front

--- src/PanierfoyenBundle/Controller/front/ProduitsController.php
+++ src/PanierfoyenBundle/Controller/front/ProduitsController.php
@@ -7,9 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Pagerfanta\Pagerfanta;
-use Pagerfanta\Adapter\DoctrineORMAdapter;
-use Pagerfanta\View\TwitterBootstrap3View;
 use PanierfoyenBundle\Entity\Produits;
 
 /**
@@ -29,43 +26,14 @@ class ProduitsController extends Controller {
         $em = $this->getDoctrine()->getManager();
         $queryBuilder = $em->getRepository('PanierfoyenBundle:Produits')->createQueryBuilder('e');
 
-//        list($produits, $pagerHtml) = $this->paginator($queryBuilder, $request);
+//        Pagination
         $paginator = $this->container->get('panierfoyen.paginator');
-        list($produits, $pagerHtml) = $paginator->paginatorSimple($queryBuilder, $request, 8, 'produits');
+        list($produits, $pagerHtml) = $paginator->paginatorSimple($queryBuilder, $request, 12, 'produits');
 
         return $this->render('produits/index.html.twig', array(
                     'produits' => $produits,
                     'pagerHtml' => $pagerHtml,
         ));
-    }
-
-    /**
-     * Get results from paginator and get paginator view.
-     *
-     */
-    protected function paginator($queryBuilder, $request) {
-        // Paginator
-        $adapter = new DoctrineORMAdapter($queryBuilder);
-        $pagerfanta = new Pagerfanta($adapter);
-        $currentPage = $request->get('page', 1);
-        $pagerfanta->setCurrentPage($currentPage);
-        $entities = $pagerfanta->getCurrentPageResults();
-
-        // Paginator - route generator
-        $me = $this;
-        $routeGenerator = function($page) use ($me) {
-            return $me->generateUrl('produits', array('page' => $page));
-        };
-
-        // Paginator - view
-        $view = new TwitterBootstrap3View();
-        $pagerHtml = $view->render($pagerfanta, $routeGenerator, array(
-            'proximity' => 3,
-            'prev_message' => 'previous',
-            'next_message' => 'next',
-        ));
-
-        return array($entities, $pagerHtml);
     }
 
     /**
