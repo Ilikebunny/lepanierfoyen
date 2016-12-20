ab73f1c0065a7c342fc0ed6013520ec84b82015f
Ilikebunny <larrue.jerome@gmail.com>
2016-12-20 21:07:03 +0100

    Fixes geocoder error
    Modified getAdressComplete() to only use Code Postal and City

--- src/PanierfoyenBundle/Entity/AbstractAdresses.php
+++ src/PanierfoyenBundle/Entity/AbstractAdresses.php
@@ -74,6 +74,7 @@ abstract class AbstractAdresses {
     public function generateAdressComplete() {
 
         $temp = $this->adr1 . ' ' . $this->adr2 . ' ' . $this->adr3 . ' ' . $this->codepostal . ' ' . $this->ville;
+        $temp = $this->codepostal . ' ' . $this->ville;
         $this->setAdressComplete($temp);
 
         return $this;
