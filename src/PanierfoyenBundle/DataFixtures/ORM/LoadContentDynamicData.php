af09b136f25dd2aae5bc8d8b27afae98ab9e17a9
Ilikebunny <larrue.jerome@gmail.com>
2017-01-02 09:52:52 +0100

    Added data fixtures for contentDynamic

---
+++ src/PanierfoyenBundle/DataFixtures/ORM/LoadContentDynamicData.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace PanierfoyenBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use PanierfoyenBundle\Entity\ContentDynamic;
+
+class LoadContentDynamicData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface {
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null) {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager) {
+
+        $import = $this->container->get('panierfoyen.importcsv');
+        $fileContent = $import->CSV_to_array('contenue.csv');
+
+        foreach ($fileContent as $numRow => $row) {
+            if ($numRow != 1) {
+                $entity = new ContentDynamic();
+                $entity->setTitre($row[0]);
+
+                $txtName = $row[1];
+                $txtContent = $import->TXT_to_String($txtName);
+                $entity->setContenue($txtContent);
+
+                $entity->setOrder($row[2]);
+                $entity->setUser($this->getReference($row[3]));
+                $entity->setPublished($row[4]);
+                
+                $publicationDate = new \DateTime($row[5]);
+                $entity->setPublicationDate($publicationDate);
+                              
+                $manager->persist($entity);
+                $manager->flush();
+            }
+        }
+    }
+
+    public function getOrder() {
+        // the order in which fixtures will be loaded
+        // the lower the number, the sooner that this fixture is loaded
+        return 13;
+    }
+
+}
