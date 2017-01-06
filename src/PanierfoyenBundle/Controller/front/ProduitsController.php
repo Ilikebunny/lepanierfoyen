f5dce9a7f5fe550b7bada03946f6df0f18adcfe9
Ilikebunny <larrue.jerome@gmail.com>
2017-01-06 20:53:55 +0100

    Added filter produits by category

--- src/PanierfoyenBundle/Controller/front/ProduitsController.php
+++ src/PanierfoyenBundle/Controller/front/ProduitsController.php
@@ -24,23 +24,67 @@ class ProduitsController extends Controller {
      */
     public function indexAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
-        $queryBuilder = $em->getRepository('PanierfoyenBundle:Produits')->createQueryBuilder('e');
-        
+
         //test
         $queryBuilder = $em->getRepository('PanierfoyenBundle:Produits')->getAllOrderedByCategory();
 
         //TEST2
         $queryBuilder2 = $em->getRepository('PanierfoyenBundle:Categories')->getAllWithProduitsAndVariations();
         $queryBuilder2 = $queryBuilder2->getResult();
-        
+
+        $categories = $em->getRepository('PanierfoyenBundle:Categories')->findAll();
+
 //        Pagination
         $paginator = $this->container->get('panierfoyen.paginator');
         list($produits, $pagerHtml) = $paginator->paginatorSimple($queryBuilder, $request, 12, 'produits');
 
         return $this->render('produits/index.html.twig', array(
                     'produits' => $produits,
+                    'categories' => $categories,
                     'pagerHtml' => $pagerHtml,
-                    'categories' => $queryBuilder2,
+                    'categories2' => $queryBuilder2,
+        ));
+    }
+
+    /**
+     * Lists all Produits entities filtered by categories
+     *
+     * @Route("/categories/{categorySelected}", name="produitsByCategories")
+     * @Method("GET")
+     */
+    public function indexCategorieAction(Request $request, $categorySelected) {
+        $em = $this->getDoctrine()->getManager();
+
+        $categories = $em->getRepository('PanierfoyenBundle:Categories')->findAll();
+        $categorySelectedObject = $em->getRepository('PanierfoyenBundle:Categories')->findOneByLibelle($categorySelected);
+
+        //temp
+        $queryBuilder = $em->getRepository('PanierfoyenBundle:Producteurs')->createQueryBuilder('a')
+                ->select('a')
+                ->leftJoin('a.category', 'c')
+                ->addSelect('c');
+
+        $queryBuilder = $queryBuilder->add('where', $queryBuilder->expr()->in('c', ':c'))
+                ->setParameter('c', $categorySelectedObject->getId())
+        ;
+
+        $queryBuilder2 = $em->getRepository('PanierfoyenBundle:Categories')->getAllWithProduitsAndVariationsFiltered($categorySelected)->getResult();
+
+        //temp
+        $routeName = 'producteursByCategories';
+
+        $paginator = $this->container->get('panierfoyen.paginator');
+        list($producteurs, $pagerHtml) = $paginator->paginatorWithParameters($queryBuilder, $request, 8, $routeName, 'categorySelected', $categorySelected);
+
+//        list($producteurs, $pagerHtml) = $this->paginatorByCategory($queryBuilder, $request, 8, $routeName, 'categorySelected', $categorySelected);
+
+        return $this->render('produits/index.html.twig', array(
+                    'producteurs' => $producteurs,
+                    'categories' => $categories,
+                    'categorySelected' => $categorySelected,
+                    'categorySelectedObject' => $categorySelectedObject,
+//                    'pagerHtml' => $pagerHtml,
+                    'categories2' => $queryBuilder2,
         ));
     }
 
