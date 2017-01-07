3e90162bd04c36afaeb87454e171043a66f9e1b2
Ilikebunny <larrue.jerome@gmail.com>
2017-01-07 11:59:00 +0100

    Added Doctrine Listener
    Generate geocode infos (prePersist)

---
+++ src/PanierfoyenBundle/Listener/DoctrineListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace PanierfoyenBundle\Listener;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use PanierfoyenBundle\Entity\AbstractAdresses;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class DoctrineListener {
+
+    private $container;
+
+    public function __construct(ContainerInterface $container = null) {
+        $this->container = $container;
+    }
+
+    /** @ORMPrePersist */
+    public function prePersist(LifecycleEventArgs $event) {
+        
+//        die('Something is being inserted!');
+
+        $entity = $event->getEntity();
+        
+        $geocoder = $this->container->get('bazinga_geocoder.geocoder');
+
+        // only act on some entity
+        if ($entity instanceof AbstractAdresses) {
+//            die('Something is being inserted!');
+            $geoResult = $geocoder->geocode($entity->getCodepostal().' '.$entity->getVille());
+             if ($geoResult->count() > 0) {
+                $address = $geoResult->first();
+                $entity->setLatitude($address->getLatitude());
+                $entity->setLongitude($address->getLongitude());
+            }
+        }
+
+    }
+
+}
