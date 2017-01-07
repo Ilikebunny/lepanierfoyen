f2f028a063d4a34f0583bd0416d446bc5f749721
Ilikebunny <larrue.jerome@gmail.com>
2017-01-07 09:58:54 +0100

    Added geocode info to entity

--- src/PanierfoyenBundle/Entity/AbstractAdresses.php
+++ src/PanierfoyenBundle/Entity/AbstractAdresses.php
@@ -53,6 +53,18 @@ abstract class AbstractAdresses {
     private $adressComplete;
 
     /**
+     * @var float
+     * @ORM\Column(name="latitude", type="float", nullable=true)
+     */
+    private $latitude;
+
+    /**
+     * @var float
+     * @ORM\Column(name="longitude", type="float", nullable=true)
+     */
+    private $longitude;
+
+    /**
      * Set adressComplete
      *
      * @param string $adressComplete
@@ -197,6 +209,50 @@ abstract class AbstractAdresses {
      */
     public function getVille() {
         return $this->ville;
+    }
+
+    /**
+     * Set latitude
+     *
+     * @param float $latitude
+     *
+     * @return AbstractAdresses
+     */
+    public function setLatitude($latitude) {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    /**
+     * Get latitude
+     *
+     * @return float
+     */
+    public function getLatitude() {
+        return $this->latitude;
+    }
+
+    /**
+     * Set longitude
+     *
+     * @param float $longitude
+     *
+     * @return AbstractAdresses
+     */
+    public function setLongitude($longitude) {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    /**
+     * Get longitude
+     *
+     * @return float
+     */
+    public function getLongitude() {
+        return $this->longitude;
     }
 
 }
